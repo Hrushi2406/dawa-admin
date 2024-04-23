9303c4b399205d2fea00e76df79de431c702225b
hrushikesh <kukularehrushi@gmail.com>
2024-04-23 16:22:01 +0530

    Fixed bug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,12 +8,6 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId:
-        "571483591189-8m1qgjp5jinlfl0s5c6s8u7ffh800353.apps.googleusercontent.com",
-      clientSecret: "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",
-    }),
-
     // ...add more providers here
   ],
 
